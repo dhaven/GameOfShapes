777214e6f695da7b248ce533568fc1d4d0d41fef
edd19 <eddy.ndizera@student.uclouvain.be>
2015-07-16 16:56:19 +0200

    Affiche les top scores

--- app/src/main/java/com/david/gameofshapes/Activities/OptionsActivity.java
+++ app/src/main/java/com/david/gameofshapes/Activities/OptionsActivity.java
@@ -1,6 +1,9 @@
 package com.david.gameofshapes.Activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.LoaderManager;
 import android.content.Context;
 import android.content.CursorLoader;
@@ -11,13 +14,18 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 import com.david.gameofshapes.Database.DbContract;
 import com.david.gameofshapes.Database.ShapesDbHelper;
+import com.david.gameofshapes.Highscore;
 import com.david.gameofshapes.Puzzle;
 import com.david.gameofshapes.R;
 
@@ -53,7 +61,43 @@ public class OptionsActivity extends Activity{
     }
 
     public void showScores(View view){
+        //Get the top 10 scores
+        ShapesDbHelper myDbHelper = new ShapesDbHelper(getApplicationContext());
+        SQLiteDatabase myDb = myDbHelper.getReadableDatabase();
+        ArrayList<Highscore> highscores =  DbContract.HighscoresTable.getTopN(myDb, 10);
 
+        //Show dialog with the scores
+        scoresDialog(highscores).show();
+
+    }
+
+    public AlertDialog scoresDialog(ArrayList<Highscore> highscores){
+        AlertDialog.Builder  builder = new AlertDialog.Builder(this);
+        builder.setTitle("Top scores");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        View view =  inflater.inflate(R.layout.dialog_options_scores, null);
+        builder.setView(view);
+
+        AlertDialog dialog = builder.create();
+
+        addScores(view, highscores);
+
+
+        return dialog;
+    }
+
+    public void addScores(View view, ArrayList<Highscore> highscores){
+        TableLayout scoresTable = (TableLayout) view.findViewById(R.id.table_scores);
+
+
+
+        for(int i = 0; i < highscores.size(); i++){
+            Highscore score = highscores.get(i);
+            TextView scoreView = (TextView) scoresTable.getChildAt(i);
+            scoreView.setText( (i+1) + ".  " + score.getName() + " - " + score.getScore());
+        }
     }
 
     public void showTutorial(View view){
